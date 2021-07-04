3f58d91a15904526f542e3ab111e796c462e3285
Antonio Muniz <antonio.gustavo_@live.com>
2021-07-04 00:18:23 -0300

    DataTable integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
-import {REACT_APP_BASE_URL} from 'utils/requests';
+import { REACT_APP_BASE_URL } from 'utils/requests';
 
 
 type ChartData = {
@@ -11,17 +11,17 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    const [chartData, setChartData] = useState < ChartData > ({labels: [], series: []})
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
     useEffect(() => {
         axios.get(`${REACT_APP_BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName)
-            const mySeries = data.map(x => x.sum)
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
 
-            setChartData({labels: myLabels, series: mySeries})
-        })
+                setChartData({ labels: myLabels, series: mySeries })
+            })
     }, [])
 
     const options = {
@@ -32,16 +32,16 @@ const DonutChart = () => {
 
     return (
         <Chart options={
-                {
-                    ... options,
-                    labels: chartData.labels
-                }
+            {
+                ...options,
+                labels: chartData.labels
             }
+        }
             series={
                 chartData.series
             }
             type="donut"
-            height="240"/>
+            height="240" />
     );
 }
 
